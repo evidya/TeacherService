b83b3d04eaf9b0ae4f6ed41d7905e913dd809f95
yogini.marathe@gmail.com <yogini.marathe@gmail.com>
2016-05-30 22:32:21 +0530

    Revert "Teacher Account save and retrieve services"
    This reverts commit bb74896ceb925fa096bce6ba0e1572047fee1989.

--- src/main/java/com/vidya/teacherServices/dao/TeacherAccountDao.java
+++
@@ -1,8 +0,0 @@
-package com.vidya.teacherServices.dao;
-
-import com.vidya.teacherServices.entities.TeacherAccount;
-
-public interface TeacherAccountDao {
-	public TeacherAccount getTeacherDetailsByID(long id) ;
-	public long saveTeacherDetails(TeacherAccount tAccount) ;
-}
