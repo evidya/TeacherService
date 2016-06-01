9cb09a69b59cf6e1a805f68ad32750145e115496
Hanimi Reddy <Hanimi@Hanimis-Mac-mini.local>
2016-05-31 22:36:29 -0400

    removed un-used variables

--- src/main/java/com/vidya/teacherServices/controller/TeacherServiceController.java
+++ src/main/java/com/vidya/teacherServices/controller/TeacherServiceController.java
@@ -1,7 +1,5 @@
 package com.vidya.teacherServices.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,27 +9,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.vidya.teacherServices.entities.TeacherAccount;
-import com.vidya.teacherServices.model.Greeting;
-
 import com.vidya.teacherServices.services.TeacherAccountService;
 
 @RestController
 public class TeacherServiceController {
-
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
     
     @Autowired
- 
 	private TeacherAccountService teacherAccountService;
 
     
-    static final Logger logger = LogManager.getLogger(TeacherServiceController.class.getName());
+    private static final Logger logger = LogManager.getLogger(TeacherServiceController.class.getName());
     
 
-    /* functoin to retrieve teacher account information based on Teacher ID */
+    /* function to retrieve teacher account information based on Teacher ID */
     @RequestMapping(value = "/getTeacher", method = RequestMethod.GET)
     @ResponseBody
     public TeacherAccount getTeacherInformation(@RequestParam(value = "id") long id) {
@@ -46,7 +37,7 @@ public class TeacherServiceController {
         return tAccount;
     }
     
-    /* functoin to create new teacher account from the first step of teacher registration */
+    /* function to create new teacher account from the first step of teacher registration */
     @RequestMapping(value = "/saveTeacherAccount", method = RequestMethod.POST)
     @ResponseBody
     public long createTeacherAccount(TeacherAccount tAccount) {
